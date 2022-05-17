852104c24ec5e7b3f6f90a848e9c415d63ab087b
Alebecito <alebe12345@gmail.com>
2022-05-16 18:17:11 -0600

    Custom Errors

--- prueba.c
+++ prueba.c
@@ -1,11 +1,7 @@
-void copyt(int n) {
-for (int i = 0 i < 100; i){
-    print("Hola\n");
-};
-// void main() {
-//     nt a = 5 + 5;
-// }
+void main() {
+    int a = 5 . 5;
+}
 
-// void main() {
-//     int a = 5 + 5
-// }
+void main() {
+    nt a = 5 + 5;
+}
